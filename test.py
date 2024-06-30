02e791e4664c49e5f139c48011ce4499b6f63ecc
ArthurLlew <kiseliov.evgeniy.iv@yandex.ru>
2024-06-30 16:20:48 +0300

    Added ctypes example code

--- test.py
+++ test.py
@@ -31,3 +31,20 @@ fft_res.real += X
 fft_res.imag += Y
 print('Python result:')
 print(fft_res)
+
+
+# NOTE: you can allways call dll explicitly with ctypes like this
+
+import ctypes as ct
+
+cudalib = ct.CDLL('./lib/cudalib/cudalib')
+# Define dll function
+cudalib.cuda_func.argtypes = np.ctypeslib.ndpointer(dtype=np.complex128, ndim=2), ct.c_size_t, ct.c_size_t
+cudalib.cuda_func.restype = None
+
+# Complex double presision array (you can even use slices when calling dll function!)
+arr2 = np.zeros((5, 4,4), dtype=np.complex128)
+# Make array C contiguous
+if not arr2.flags['C_CONTIGUOUS']:
+    arr2 = np.ascontiguousarray(arr2)
+cudalib.cuda_func(arr2[0, :, :], *arr.shape)
